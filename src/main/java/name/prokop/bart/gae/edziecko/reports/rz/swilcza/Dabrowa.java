b9cb16a1f7ce9d72a5a9c08b2c75ac1be4e61d21
Bart Prokop <prokop.bart@gmail.com>
2013-09-23 18:22:53 +0200

    Dabrowa zmiany

--- src/main/java/name/prokop/bart/gae/edziecko/reports/rz/swilcza/Dabrowa.java
+++ src/main/java/name/prokop/bart/gae/edziecko/reports/rz/swilcza/Dabrowa.java
@@ -35,6 +35,14 @@ public class Dabrowa extends DzienPobytuDziecka {
         COLTYPE.put(ZYWIENIE, TypKolumny.Kwota);
         COLTYPE.put(POBYT, TypKolumny.Kwota);
     }
+    private final Date T_06_30;
+    private final Date T_07_00;
+    private final Date T_07_30;
+    private final Date T_08_00;
+    private final Date T_13_00;
+    private final Date T_14_00;
+    private final Date T_15_00;
+    private final Date T_16_00;
     private final Date TBEGF;
     private final Date TENDF;
     private final Date TBEGD;
@@ -46,22 +54,30 @@ public class Dabrowa extends DzienPobytuDziecka {
     public Dabrowa(KidsReport raport, Dziecko dziecko, Date date, NavigableSet<Zdarzenie> entries, Map<String, Object> shared) {
         super(raport, dziecko, date, entries, shared);
 
-        TBEGD = DateToolbox.getBeginingOfDay(date, 06 * HOUR + 30 * MIN);
         TMIDD = DateToolbox.getBeginingOfDay(date, 11 * HOUR + 15 * MIN);
-        TENDD = DateToolbox.getBeginingOfDay(date, 16 * HOUR + 30 * MIN);
+
+        T_06_30 = DateToolbox.getBeginingOfDay(date, 6 * HOUR + 15 * MIN);
+        T_07_00 = DateToolbox.getBeginingOfDay(date, 7 * HOUR + 15 * MIN);
+        T_07_30 = DateToolbox.getBeginingOfDay(date, 7 * HOUR + 15 * MIN);
+        T_08_00 = DateToolbox.getBeginingOfDay(date, 8 * HOUR + 15 * MIN);
+        T_13_00 = DateToolbox.getBeginingOfDay(date, 13 * HOUR + 15 * MIN);
+        T_14_00 = DateToolbox.getBeginingOfDay(date, 14 * HOUR + 15 * MIN);
+        T_15_00 = DateToolbox.getBeginingOfDay(date, 15 * HOUR + 15 * MIN);
+        T_16_00 = DateToolbox.getBeginingOfDay(date, 16 * HOUR + 15 * MIN);
 
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeZone(TimeZone.getTimeZone("CET"));
         calendar.setTime(date);
 
         dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
-        if (dayOfWeek == Calendar.MONDAY || dayOfWeek == Calendar.TUESDAY) {
+        if (dayOfWeek == Calendar.TUESDAY || dayOfWeek == Calendar.FRIDAY) {
             TBEGF = DateToolbox.getBeginingOfDay(date, 7 * HOUR + 30 * MIN);
         } else {
-            TBEGF = DateToolbox.getBeginingOfDay(date, 8 * HOUR + 0 * MIN);
+            TBEGF = DateToolbox.getBeginingOfDay(date, 8 * HOUR + 00 * MIN);
         }
-        TENDF = DateToolbox.getBeginingOfDay(date, 13 * HOUR + 0 * MIN);
-
+        TENDF = DateToolbox.getBeginingOfDay(date, 13 * HOUR + 00 * MIN);
+        TBEGD = DateToolbox.getBeginingOfDay(date, 6 * HOUR + 30 * MIN);
+        TENDD = DateToolbox.getBeginingOfDay(date, 16 * HOUR + 00 * MIN);
         calc();
     }
 
@@ -69,7 +85,7 @@ public class Dabrowa extends DzienPobytuDziecka {
         if (czasOd.equals(czasDo)) {
             if (czasOd.before(TMIDD)) {
                 czasDo = TENDD;
-                uwagi = "Brak wieczora (ustalono 16:30)";
+                uwagi = "Brak wieczora (ustalono 16:00)";
             } else {
                 czasOd = TBEGD;
                 uwagi = "Brak poranku (ustalono 6:30)";
@@ -97,13 +113,42 @@ public class Dabrowa extends DzienPobytuDziecka {
         }
 
         double opieka = 0.0;
-        if (czasPrzed > 0.0) {
-            opieka += ((czasPrzed - 1) / HALF_HOUR + 1) * 1;
+        if (dayOfWeek == Calendar.TUESDAY || dayOfWeek == Calendar.FRIDAY) {
+            if (czasOd.before(T_07_00)) {
+                opieka = +0.5;
+            }
+            if (czasOd.before(T_07_30)) {
+                opieka = +0.5;
+            }
+            if (czasDo.after(T_13_00)) {
+                opieka = +1.0;
+            }
+            if (czasDo.after(T_14_00)) {
+                opieka = +1.0;
+            }
+            if (czasDo.after(T_15_00)) {
+                opieka = +1.0;
+            }
+        } else {
+            if (czasOd.before(T_07_00)) {
+                opieka = +0.5;
+            }
+            if (czasOd.before(T_07_30)) {
+                opieka = +0.5;
+            }
+            if (czasOd.before(T_08_00)) {
+                opieka = +0.5;
+            }
+            if (czasDo.after(T_13_00)) {
+                opieka = +1.0;
+            }
+            if (czasDo.after(T_14_00)) {
+                opieka = +1.0;
+            }
+            if (czasDo.after(T_15_00)) {
+                opieka = +1.0;
+            }
         }
-        if (czasPo > 0) {
-            opieka += ((czasPo - 1) / HALF_HOUR + 1) * 1;
-        }
-
 
         opieka = BPMath.roundCurrency(opieka * dziecko.getRabat1AsFactor());
         double zywienie = 3.0;
